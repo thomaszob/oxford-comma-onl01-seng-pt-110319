7f5996e0f8db1a5514e9bed0fd2a05cfa1c62104
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-15 01:59:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
+  finalString = ""
+  if array.length == 1
+    finalString = "#{array[0]}"
+  
+  elsif array.length == 2 
+    finalString = "#{array[0]} and #{array[1]}"
+    
+  else
+    finalString = "#{array[0]}"
+    #counter = 1 
+    #array.shift()
+    #array.each{|fruit|
+    #  if counter < array.length
+    #   finalString << ", #{fruit}"
+    #  end
+    
+    #}
+    #finalString << "and #{array[-1]}"
+    finalString = array[0..-2].join(", ") + ", and " + array[-1]
+  end
+  finalString
 end
+
