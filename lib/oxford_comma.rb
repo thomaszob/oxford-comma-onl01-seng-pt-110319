5c9065315e8e2d9102d76191f85a629157265070
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-15 01:38:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  finalString = ""
+  if array.length == 2 
+    finalString = "#{array[0]} and #{array[1]}"
+  
+  else 
+    
+    counter = 1 
+    array.each{|fruit|
+      if counter < array.length
+        finalString << ",#{fruit}"
+      end
+    #finalString << "and #{fruit}"
+    }
+  end
+  finalString
 end
